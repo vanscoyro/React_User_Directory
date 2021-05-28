02f41760ba628fd3711f8a6bd385a00e6cb0e3d5
Robert VanScoy <rvans003@fiu.edu>
2021-05-28 10:53:17 -0400

    added component that will display DataBody rows inside a main table

---
+++ src/components/DataTable.js
@@ -0,0 +1,36 @@
+import React from "react";
+import DataBody from "./DataBody";
+
+
+function DataTable({ headings, users, handleSort}) {
+    return (
+        <div className="datatable mt-5">
+            <table
+                id="table"
+                className="table table-striped table-hover table-condensed">
+                    <thead class="hed">
+                        <tr>
+                            {headings.map(({ name, width }) => {
+                                return (
+                                    <th className="col"
+                                        key={name}
+                                        style={{ width }}
+                                        onClick={() => {
+                                            handleSort(name.toLowerCase());
+                                        }}
+                                        >
+                                            {name}
+                                            <span className="pointer"><i class="fas fa-sort"></i></span>
+                                    </th>
+                                );
+                            })}
+                        </tr>
+                    </thead>
+
+                <DataBody users={users} />
+            </table>
+        </div>
+    );
+}
+
+export default DataTable;
